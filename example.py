306bc234ad8f6c1fd2044a03e1300842886ab485
龙州客 <liujiantong@gmail.com>
2023-03-26 10:46:41 +0800

    Reduce the value of batch_size

--- example.py
+++ example.py
@@ -69,7 +69,8 @@ def main(
     temperature: float = 0.8,
     top_p: float = 0.95,
     max_seq_len: int = 512,
-    max_batch_size: int = 32,
+    max_batch_size: int = 16,
+    # max_batch_size: int = 32,
 ):
     local_rank, world_size = setup_model_parallel()
     if local_rank > 0:
