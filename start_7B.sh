7fbcdb6ab6033e9bc9a088a660b711c5e9141948
龙州客 <liujiantong@gmail.com>
2023-04-01 13:08:26 +0800

    CUDA_VISIBLE_DEVICES

--- start_7B.sh
+++ start_7B.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-OMP_NUM_THREADS=2 torchrun --nproc_per_node 2 example1.py --max_batch_size 10 --ckpt_dir ~/ai/llama-models/7B_MP2/ --tokenizer_path ~/ai/llama-models/tokenizer.model
+CUDA_VISIBLE_DEVICES=1,2 OMP_NUM_THREADS=2 torchrun --nproc_per_node 2 example1.py --max_batch_size 10 --ckpt_dir ~/ai/llama-models/7B_MP2/ --tokenizer_path ~/ai/llama-models/tokenizer.model
