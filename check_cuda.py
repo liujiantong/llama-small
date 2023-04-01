337a366e6e395fbf1ffdb0b0972971e31cd9f927
龙州客 <liujiantong@gmail.com>
2023-04-01 16:45:21 +0800

    refined

--- check_cuda.py
+++ check_cuda.py
@@ -9,5 +9,4 @@ num_gpus = torch.cuda.device_count()
 print(f"Number of available GPUs: {num_gpus}")
 
 for i in range(num_gpus):
-    device = torch.device(f"cuda:{i}")
-    print(torch.cuda.get_device_properties(device))
+    print(f"cuda:{i}:{torch.cuda.get_device_properties(i)}")
